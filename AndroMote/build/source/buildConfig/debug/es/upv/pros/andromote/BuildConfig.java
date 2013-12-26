73e963049c859d6d9ee43490411bee674c07bd61
Bernardo Botella Corbí <estemaci@gmail.com>
2013-12-26 18:21:45 +0100

    Ready to push repo

--- AndroMote/build/source/buildConfig/debug/es/upv/pros/andromote/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package es.upv.pros.andromote;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
--- AndroMote/src/main/java/es/upv/pros/andromote/agenthanlersinterfaces/IAgentHandler.java
+++
@@ -1,9 +0,0 @@
-package es.upv.pros.andromote.agenthanlersinterfaces;
-
-/**
- * Created by bbotella on 21/08/13.
- */
-public interface IAgentHandler {
-    public void handleMessage();
-    public void sendAck();
-}
